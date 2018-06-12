fb9f76caf86543f67d56b86d6cb2047e78f92370
peacecop kalmer: <pandeero@gmail.com>
2018-06-12 13:10:36 +0300

    red - yellow - blinking green

--- WebContent/js/traffic-lights.js
+++ WebContent/js/traffic-lights.js
@@ -2,34 +2,74 @@
  * 
  */
 class TrafficLights {
-	constructor(){
-		this.light = "red";
-	}
-	turnRed(){
-		document.querySelector("div.Red").style.backgroundColor = "hsla(0, 100%, 50%, 1)";
-		document.querySelector("div.Yellow").style.backgroundColor = "hsla(0, 100%, 0%, 1)";
-		document.querySelector("div.Green").style.backgroundColor = "hsla(0, 100%, 0%, 1)";
-		setTimeout(() => {
-			this.turnYellow()
-		}, 3000);
-	}
-	turnYellow(){
-		clearTimeout(this.timeout);
-		document.querySelector("div.Red").style.backgroundColor = "hsla(0, 100%, 0%, 1)";
-		document.querySelector("div.Yellow").style.backgroundColor = "hsla(60, 100%, 50%, 1)";
-		document.querySelector("div.Green").style.backgroundColor = "hsla(60, 100%, 0%, 1)";
-		setTimeout(() => {
-			this.turnGreen()
-		}, 3000);
-	}
-	turnGreen() {
-		document.querySelector("div.Red").style.backgroundColor = "hsla(0, 100%, 0%, 1)";
-		document.querySelector("div.Yellow").style.backgroundColor = "hsla(60, 100%, 0%, 1)";
-		document.querySelector("div.Green").style.backgroundColor = "hsla(120, 100%, 50%, 1)";
-		setTimeout(() => {
-			this.turnYellow()
-		}, 3000);
+	turn(trafficLights){
+		switch(trafficLights.color){
+		case "red":{
+			document.querySelector("div.Red").style.backgroundColor = "hsla(0, 100%, 50%, 1)";
+			document.querySelector("div.Yellow").style.backgroundColor = "hsla(0, 100%, 0%, 1)";
+			document.querySelector("div.Green").style.backgroundColor = "hsla(0, 100%, 0%, 1)";
+			setTimeout(() => {
+				this.turn({
+					"color": "yellow",
+					"toGreen": true
+				})
+			}, 3000);
+			break;
+		}
+		case "yellow":
+			{
+			document.querySelector("div.Red").style.backgroundColor = "hsla(0, 100%, 0%, 1)";
+			document.querySelector("div.Yellow").style.backgroundColor = "hsla(60, 100%, 50%, 1)";
+			document.querySelector("div.Green").style.backgroundColor = "hsla(60, 100%, 0%, 1)";
+			if(trafficLights.toGreen){
+				setTimeout(() => {
+					this.turn({
+						"color": "green",
+						"isBlinking": false
+					})
+				}, 3000);
+			} else {
+				setTimeout(() => {
+					this.turn({
+						"color": "red"
+					})
+				}, 3000);
+			}
+			break;
+			}
+		case "green":{
+			document.querySelector("div.Red").style.backgroundColor = "hsla(0, 100%, 0%, 1)";
+			document.querySelector("div.Yellow").style.backgroundColor = "hsla(60, 100%, 0%, 1)";
+			document.querySelector("div.Green").style.backgroundColor = "hsla(120, 100%, 50%, 1)";
+			let timeout = trafficLights.isBlinking ? 1000 : 3000;
+			setTimeout(() => {
+				this.turn({
+					"color": "black",
+					"toGreen":false
+				})
+			}, timeout);
+			break;
+		}
+		case "black":{
+			document.querySelector("div.Red").style.backgroundColor = "hsla(0, 100%, 0%, 1)";
+			document.querySelector("div.Yellow").style.backgroundColor = "hsla(60, 100%, 0%, 1)";
+			document.querySelector("div.Green").style.backgroundColor = "hsla(120, 100%, 0%, 1)";
+			setTimeout(() => {
+				this.turn({
+					"color": "green",
+					"isBlinking": true,
+					"toGreen":false
+				})
+			}, 1000);
+			break;
+		}
+		default:{
+			console.log("blinking yellow");
+		}
+		}
 	}
 }
 trafficLights = new TrafficLights();
-trafficLights.turnRed();
+trafficLights.turn({
+	"color": "red"
+});
