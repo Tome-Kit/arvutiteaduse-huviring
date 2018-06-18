552e7e7a3dccd0ec8176707854adb97ae0b36b32
peacecop-kalmer <peacecop-kalmer@peacecop-VirtualBox>
2018-06-18 21:06:00 +0300

    after blinking green, turns to yellow

--- WebContent/js/traffic-lights.js
+++ WebContent/js/traffic-lights.js
@@ -42,18 +42,32 @@ class TrafficLights {
 			document.querySelector("div.Yellow").style.backgroundColor = "hsla(60, 100%, 0%, 1)";
 			document.querySelector("div.Green").style.backgroundColor = "hsla(120, 100%, 50%, 1)";
 			let timeout = trafficLights.isBlinking ? 1000 : 3000;
-			setTimeout(() => {
-				this.turn({
-					"color": "black",
-					"toGreen":false
-				})
-			}, timeout);
+			if (TrafficLights.counter > 2) {
+				setTimeout(() => {
+					this.turn({
+						"color": "yellow",
+						"toGreen":false
+					})
+				}, timeout);
+			} else {
+				setTimeout(() => {
+					this.turn({
+						"color": "black",
+						"toGreen":false
+					})
+				}, timeout);
+			}
 			break;
 		}
 		case "black":{
 			document.querySelector("div.Red").style.backgroundColor = "hsla(0, 100%, 0%, 1)";
 			document.querySelector("div.Yellow").style.backgroundColor = "hsla(60, 100%, 0%, 1)";
 			document.querySelector("div.Green").style.backgroundColor = "hsla(120, 100%, 0%, 1)";
+			if (undefined === TrafficLights.counter) {
+				TrafficLights.counter = 1;
+			} else {
+				TrafficLights.counter ++;
+			}
 			setTimeout(() => {
 				this.turn({
 					"color": "green",
