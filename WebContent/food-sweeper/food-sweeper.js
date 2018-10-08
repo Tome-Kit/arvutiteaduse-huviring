77fd41bc086fea066fc2057e7dd259c2c4576a4a
peacecop kalmer <pandeero@gmail.com>
2018-10-08 23:08:33 +0300

    initial example how to build an overlay

---
+++ WebContent/food-sweeper/food-sweeper.js
@@ -0,0 +1,11 @@
+/**
+ * 
+ */
+document.getElementById("point1").onclick = function(){
+	console.log(" 15 ");
+	document.getElementById("point1").style.visibility = "hidden";
+}
+document.getElementById("point4").onclick = function(){
+	console.log(" 9 ");
+	document.getElementById("point4").style.backgroundColor= "#dadada";
+}
