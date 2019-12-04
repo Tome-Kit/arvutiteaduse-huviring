f0fc0901cb4c7e5a80866c8b41e7cbf7b8764672
karl <karlvalterp@gmail.com>
2019-12-04 16:13:31 +0200

    nime pikkus

--- karl/phyton/phyton.js
+++ karl/phyton/phyton.js
@@ -1,1 +1,16 @@
 print("Tere")
+
+name = input("Mis on sinu nimi? ")
+
+print("Kas su nimi on tõesti " + name + "?")
+if len(name) > 10:
+    print("Sul on pikk nimi.")
+else:
+    print("Sul on lühike nimi.")
+lengthOfNAme = len(name)
+if lengthOfNAme > 10:
+    print("Sul on pikk nimi.")
+elif lengthOfNAme > 5:
+      print("Sul on keskmise pikkusega nimi.")
+else:
+    print("Sul on lühike nimi.")
